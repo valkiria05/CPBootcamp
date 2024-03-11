e8173b4c487c44dba527ad0b8527957381c6fc82
valkiria05 <162150378+valkiria05@users.noreply.github.com>
2024-03-11 10:26:43 +0100

    Create i.java

---
+++ Semana1/i.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class i {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        long m = scanner.nextLong();
+        System.out.println(m*(m+1)/2);
+    }
+}
